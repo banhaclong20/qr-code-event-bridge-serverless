fbafc9ddcad014bfaea4be89375415d91e9611c8
Tom Tran <banhaclong20@gmail.com>
2023-05-03 16:59:01 -0500

    implemented batch process trigger from Lambda

---
+++ functions/processBatchEvent.js
@@ -0,0 +1,24 @@
+module.exports.handler = async (event) => {
+  const records = event.Records;
+  let batchItemFailures = [];
+
+  if (records.length) {
+    for (const record of records) {
+      try {
+        const parsedBody = JSON.parse(record.body);
+        if (typeof parsedBody.detail.vehicleNo !== 'string') {
+          throw new Error('Vehicle Number Must be a String');
+        }
+        console.log(
+          'processing vechile details ' + parsedBody.detail.vehicleNo
+        );
+        console.log('processing is successful ' + record.messageId);
+      } catch (err) {
+        batchItemFailures.push({
+          itemIdentifier: record.messageId,
+        });
+      }
+    }
+  }
+  return { batchItemFailures };
+};
