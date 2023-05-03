3598bdecc456339509ff689b922863a00e98ccf5
Tom Tran <banhaclong20@gmail.com>
2023-05-03 12:36:29 -0500

    Send event to eventBridge

--- functions/generateEvent.js
+++ functions/generateEvent.js
@@ -1,8 +1,27 @@
 'use strict';
 
+const EventBridge = require('aws-sdk/clients/eventbridge');
+const { EVENT_BUS_NAME } = process.env;
+const eventBridge = new EventBridge();
+
 module.exports.handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify('running'),
+  const body = JSON.parse(event.body);
+  const entry = {
+    EventBusName: EVENT_BUS_NAME,
+    Detail: JSON.stringify({
+      vehicleNo: body.vehicleNo,
+      NIC: body.nic,
+    }),
+    Source: 'fuel-app',
+    DetailType: 'user-signup',
   };
+  try {
+    const output = await eventBridge.putEvents({ Entries: [entry] }).promise();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(output),
+    };
+  } catch (err) {
+    console.log(err);
+  }
 };
